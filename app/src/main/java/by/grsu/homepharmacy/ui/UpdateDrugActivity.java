97aeccd96feeffdca3b78b1066d6ed89dbc101d5
neverherox <kirillromashkevich@gmail.com>
2020-12-11 23:36:40 +0300

    small fix

--- app/src/main/java/by/grsu/homepharmacy/ui/UpdateDrugActivity.java
+++ app/src/main/java/by/grsu/homepharmacy/ui/UpdateDrugActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.DialogFragment;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -59,7 +60,6 @@ public class UpdateDrugActivity extends AppCompatActivity {
 
         drug.setName(drugName.getText().toString());
         drug.setDescription(drugDescription.getText().toString());
-
         drug.setForm(Form.valueOf(form));
 
         Intent intent = new Intent();
@@ -70,5 +70,23 @@ public class UpdateDrugActivity extends AppCompatActivity {
     public void showDatePickerDialog(View v) {
         newFragment = new DatePickerFragment(drug);
         newFragment.show(getSupportFragmentManager(), "datePicker");
+    }
+    public void insertImage(View v)
+    {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_OPEN_DOCUMENT);
+        startActivityForResult(Intent.createChooser(intent, "select a picture"), 1);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1) {
+            Uri selectedImageUri;
+            if(resultCode == RESULT_OK) {
+                selectedImageUri = data.getData();
+                drug.setImageUri(selectedImageUri.toString());
+            }
+        }
     }
 }
